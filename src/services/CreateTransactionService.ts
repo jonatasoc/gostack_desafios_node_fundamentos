9d4a6f7702f8b750904d2d30702cac892814e3e4
Jonatas de Oliveira Coelho <jonatas.oliveiracoelho@gmail.com>
2020-09-03 00:01:54 -0300

    Challange Finished

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,27 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    const balance = this.transactionsRepository.getBalance();
+
+    if (type === 'income') {
+      balance.income += value;
+    } else {
+      // Tratando caso de outcome ser maior do que o balance total
+      if (balance.total < value)
+        throw Error('Balance is not enough for this operation');
+      balance.outcome += value;
+    }
+
+    balance.total = balance.income - balance.outcome;
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
